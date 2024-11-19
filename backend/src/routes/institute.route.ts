1c6fa169b057c78e8a7cad2d83539a3ffeb0f42e
JustPowerful <ahmedaminedoudech@gmail.com>
2024-11-19 23:18:05 +0100

    feat: add pagination & delete to institute route

--- backend/src/routes/institute.route.ts
+++ backend/src/routes/institute.route.ts
@@ -1,50 +1,198 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { instituteInsertSchema, institutesTable } from "../db/schema";
 import { authPlugin } from "../plugins/auth.plugin";
 import { db } from "../db";
-import { eq } from "drizzle-orm";
+import { eq, ilike } from "drizzle-orm";
 
-const instituteRoute = new Elysia({
+const router = new Elysia({
   prefix: "/institute",
-}).use(authPlugin);
+});
 
-instituteRoute.post(
-  "/create",
-  async ({ body, user, set }) => {
-    const { name, city } = body;
-    if (user?.role !== "admin") {
-      set.status = 401; // Unauthorized
-      return {
-        message: "Unauthorized",
-      };
-    }
+// routes that do not require authentication
+router.get(
+  "/paginate",
+  async ({ query, set }) => {
+    try {
+      const { page, limit, search } = query;
 
-    await db.insert(institutesTable).values({
-      name,
-      city,
-    });
-
-    const institute = (
-      await db
+      const institutes = await db
         .select()
         .from(institutesTable)
-        .where(eq(institutesTable.name, name))
-    )[0];
+        .where(ilike(institutesTable.name, `%${search}%`))
+        .limit(limit)
+        .offset((page - 1) * limit);
 
-    if (!institute) {
       return {
-        message: "Institute not found",
+        message: "Successfully fetched institutes",
+        institutes: institutes,
+      };
+    } catch (error) {
+      set.status = 500;
+      return {
+        message: "Internal server error",
       };
     }
-
-    return {
-      message: "Institute created",
-      institute,
-    };
   },
   {
-    body: instituteInsertSchema,
+    query: t.Object({
+      page: t.Number({ default: 1 }),
+      limit: t.Number({ default: 10 }),
+      search: t.String({ default: "" }),
+    }),
   }
 );
 
-export default instituteRoute;
+// routes that require authentication
+router
+  .use(authPlugin)
+  .post(
+    "/create",
+    async ({ body, user, set }) => {
+      try {
+        const { name, city } = body;
+        if (user?.role !== "admin") {
+          set.status = 401; // Unauthorized
+          return {
+            message: "Unauthorized",
+          };
+        }
+
+        await db.insert(institutesTable).values({
+          name,
+          city,
+        });
+
+        const institute = (
+          await db
+            .select()
+            .from(institutesTable)
+            .where(eq(institutesTable.name, name))
+        )[0];
+
+        if (!institute) {
+          return {
+            message: "Institute not found",
+          };
+        }
+
+        return {
+          message: "Institute created",
+          institute,
+        };
+      } catch (error) {
+        set.status = 500;
+        return {
+          message: "Internal server error",
+        };
+      }
+    },
+    {
+      body: instituteInsertSchema,
+    }
+  )
+  .post(
+    "/create",
+    async ({ body, user, set }) => {
+      try {
+        const { name, city } = body;
+        if (user?.role !== "admin") {
+          set.status = 401; // Unauthorized
+          return {
+            message: "Unauthorized",
+          };
+        }
+
+        await db.insert(institutesTable).values({
+          name,
+          city,
+        });
+
+        const institute = (
+          await db
+            .select()
+            .from(institutesTable)
+            .where(eq(institutesTable.name, name))
+        )[0];
+
+        if (!institute) {
+          return {
+            message: "Institute not found",
+          };
+        }
+
+        return {
+          message: "Institute created",
+          institute,
+        };
+      } catch (error) {
+        set.status = 500;
+        return {
+          message: "Internal server error",
+        };
+      }
+    },
+    {
+      body: instituteInsertSchema,
+    }
+  )
+  .delete(
+    "/delete",
+    async ({ body, set, user }) => {
+      try {
+        const { id } = body;
+        if (user?.role !== "admin") {
+          return {
+            message: "Unauthorized",
+          };
+        }
+
+        await db.delete(institutesTable).where(eq(institutesTable.id, id));
+        return {
+          message: "Institute deleted",
+        };
+      } catch (error) {
+        set.status = 500;
+        return {
+          messsage: "Internal server error",
+        };
+      }
+    },
+    {
+      body: t.Object({
+        id: t.String(),
+      }),
+    }
+  )
+  .patch(
+    "/update/:id",
+    async ({ body, user, set, params: { id } }) => {
+      try {
+        const { name, city } = body;
+        if (user?.role !== "admin") {
+          return {
+            message: "Unauthorized",
+          };
+        }
+        const institute = await db
+          .update(institutesTable)
+          .set({
+            name,
+            city,
+          })
+          .returning();
+
+        return {
+          message: "Successfully updated institute",
+          institute: institute[0],
+        };
+      } catch (error) {
+        set.status = 500;
+        return {
+          message: "Internal server error",
+        };
+      }
+    },
+    { body: instituteInsertSchema, params: t.Object({ id: t.String() }) }
+  );
+
+export default router;
