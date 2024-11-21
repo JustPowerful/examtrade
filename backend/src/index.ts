2ae3962035c3c109f25793e0db93059b9292c5ee
JustPowerful <ahmedaminedoudech@gmail.com>
2024-11-21 20:57:10 +0100

    feat: serve static files from the public folder + check service health

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
-import { Elysia, Context } from "elysia";
+import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
 
 const app = new Elysia();
 
@@ -6,6 +7,16 @@ const app = new Elysia();
 import authRoute from "./routes/auth.route";
 import instituteRoute from "./routes/institute.route";
 import documentRoute from "./routes/document.route";
+
+app.use(staticPlugin());
+
+app.get("/health", () => {
+  return {
+    healthy: true,
+    version: "1.0.0",
+    message: "Backend service is healthy",
+  };
+});
 
 app.group("/api", (app) => {
   return app.use(authRoute).use(instituteRoute).use(documentRoute);
