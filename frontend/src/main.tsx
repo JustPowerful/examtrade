a5d989791f322ab5527a38ac2d101b419f7ee4e7
JustPowerful <ahmedaminedoudech@gmail.com>
2024-12-16 00:39:36 +0100

    feat: implement authentication loader for protected routes and add Upload page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Home";
+import "@/index.css";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import QueryProvider from "./components/providers/QueryProvider";
-import Layout from "./components/global/Layout";
-import Dashboard from "./pages/Dashboard";
-import Browse from "./pages/Browse";
+import QueryProvider from "@/components/providers/QueryProvider";
+import Layout from "@/components/global/Layout";
+import Dashboard from "@/pages/Dashboard";
+import Browse from "@/pages/Browse";
+import Upload from "@/pages/Upload";
+import Home from "@/Home";
+
+const authLoader = async () => {
+  try {
+    const response = await fetch("/api/auth/validate", {
+      method: "POST",
+    });
+    if (response.ok) {
+      return null;
+    } else {
+      throw redirect("/");
+    }
+  } catch (error) {
+    throw redirect("/");
+  }
+};
 
 const router = createBrowserRouter([
   {
@@ -15,8 +35,9 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/dashboard", element: <Dashboard /> },
-      { path: "/browse", element: <Browse /> },
+      { path: "/dashboard", element: <Dashboard />, loader: authLoader },
+      { path: "/browse", element: <Browse />, loader: authLoader },
+      { path: "/upload", element: <Upload />, loader: authLoader },
     ],
   },
 ]);
